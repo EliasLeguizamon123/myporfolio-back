66e42f221abdc920a1a03e02e046cb6f9fe26598
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2022-09-08 16:40:40 -0300

    update dependencies and fix some deprecated code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 async function main() {
   await app.listen(process.env.PORT || 5000, () =>{
     console.log('====================================');
-    console.log(`App Running on http://localhost:${process.env.PORT}`);
+    process.env.PORT ? console.log(`App Running on http://localhost:${process.env.PORT}`) : console.log(`App Running on http://localhost:5000`);
     console.log('====================================');
   })
 }
